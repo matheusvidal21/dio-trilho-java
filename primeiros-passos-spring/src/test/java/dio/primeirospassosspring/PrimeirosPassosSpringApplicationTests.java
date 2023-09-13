e3b852db6c70157c6ef29d112b8283fbe6d85fed
matheusvidal21 <matheusvidal140@gmail.com>
2023-09-12 22:58:50 -0300

    Primeiros passos com Spring Boot

---
+++ primeiros-passos-spring/src/test/java/dio/primeirospassosspring/PrimeirosPassosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.primeirospassosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
