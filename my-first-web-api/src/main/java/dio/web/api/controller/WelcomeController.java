e647175a73ffd3a7c6fcf7c3011f2f84f752ae29
matheusvidal21 <matheusvidal140@gmail.com>
2023-09-18 11:37:37 -0300

    <3

---
+++ my-first-web-api/src/main/java/dio/web/api/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package dio.web.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to my Spring Booy Web API";
+    }
+
+
+}
